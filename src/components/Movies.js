210fca18895b81e98dc3704dec57a100860f8ff8
max101g <maxgithinji101@gmail.com>
2023-01-22 05:02:44 +0300

    update react

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  // const movieTime = movies.time.map((time) => (
+  //   <p key={time}>{time}</p>
+  // ))
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <div>
+        {movies.map((movie) => (
+          <div key={movie.title}>
+            <h3>{movie.title}</h3>
+            <p key={movie.time}>{movie.time}</p>
+            <ul>
+              {movie.genres.map((genre) => (
+                <li key={genre}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
+
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+          <nav>
+              <div className="navbar">{/*{code here}*/}
+                <NavLink exact to="/" style={{paddingRight:"35px"}}>Home</NavLink>
+                <NavLink to="/movies" style={{paddingRight:"35px"}}>Movies</NavLink>
+                <NavLink to="/actors" style={{paddingRight:"35px"}}>Actors</NavLink>
+                <NavLink to="/directors" style={{paddingRight:"35px"}}>Directors</NavLink>
+              </div>
+          </nav>
+         
+         )
 }
 
 export default NavBar;
